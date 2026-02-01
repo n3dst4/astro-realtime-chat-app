dc7ca2fb75a003e889c59eba45a4231588cb67cd
Neil de Carteret <git@lumphammer.net>
2026-02-01 15:09:39 +0000

    counter api and page

---
+++ src/pages/counter/api/[counterId]/decrement.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from "astro";
+import { env } from "cloudflare:workers";
+
+export const POST: APIRoute = async ({ params, request, locals: _locals }) => {
+  const { counterId } = params;
+  if (!counterId) return new Response("Room ID is required", { status: 400 });
+  // Get the ChatRoom Durable Object namespace
+  const CounterNamespace = env.Counter;
+  if (!CounterNamespace)
+    return new Response("Chat room binding not found", { status: 500 });
+  // Get a Durable Object ID for this room
+  // idFromName ensures the same room ID always maps to the same Durable Object
+  const durableObjectId = CounterNamespace.idFromName(counterId);
+  // Get a stub (reference) to the Durable Object
+  const durableObjectStub = CounterNamespace.get(durableObjectId);
+  const newValue = await durableObjectStub.decrement();
+  return new Response(`${newValue}`);
+};
