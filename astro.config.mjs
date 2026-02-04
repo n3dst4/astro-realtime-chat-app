516787f484684ada173e9dfe5843a65f61e1d292
Neil de Carteret <git@lumphammer.net>
2026-02-04 18:14:32 +0000

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import cloudflare from "@astrojs/cloudflare";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   output: "static",
   adapter: cloudflare({}),
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
