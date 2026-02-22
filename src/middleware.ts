10bde79f2f3b6b47d90fdb2509560adea2b04c8f
Neil de Carteret <git@lumphammer.net>
2026-02-22 12:27:18 +0000

    how to run worker for static assets

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,51 @@
 import { defineMiddleware, sequence } from "astro:middleware";
 
-const addPTerryHeader = defineMiddleware(async (_context, next) => {
-  const response = await next();
+const log = console.log.bind(console, "[middleware]");
+
+const answers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes – definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don’t count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
+export function addPotatoHeader(request: Request, response: Response) {
   if (response.status === 101) {
-    console.log("websocket - stepping back");
+    log("websocket - stepping back");
+    return response;
   } else {
-    response.headers.set("X-Clacks-Overhead", "GNU Terry Pratchett");
-    console.log("set header");
+    log("setting header for", request.url);
+    // clone the response to avoid "TypeError: Can't modify immutable headers"
+    const response2 = new Response(response.body, response);
+    response2.headers.set(
+      "X-Are-The-Potato-People-Watching",
+      answers[Math.floor(Math.random() * 20)],
+    );
+    return response2;
   }
-  return response;
+}
+
+// as of writing, we also do this in the worker (so it covers static assets
+// covered by `assets.run_worker_first`) but I'm leaving this here as an example
+// of middleware.
+const addPTerryHeaderMiddleware = defineMiddleware(async (_context, next) => {
+  return addPotatoHeader(_context.request, await next());
 });
 
-export const onRequest = sequence(addPTerryHeader);
+export const onRequest = sequence(addPTerryHeaderMiddleware);
--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -4,9 +4,37 @@ export { ChatRoom } from "./ChatRoom";
 export { Counter } from "./Counter";
 export { Roller } from "./Roller";
 
+const log = console.log.bind(console, "[worker]");
+
+export function addPTerryHeader(request: Request, response: Response) {
+  if (response.status === 101) {
+    log("websocket - stepping back");
+    return response;
+  } else {
+    log("setting header for", request.url);
+    // clone the response to avoid "TypeError: Can't modify immutable headers"
+    const response2 = new Response(response.body, response);
+    response2.headers.set("X-Clacks-Overhead", "GNU Terry Pratchett");
+    return response2;
+  }
+}
+
 export default {
   async fetch(request, env, ctx) {
-    return handler.fetch(request, env, ctx);
+    const response = await handler.fetch(request, env, ctx);
+    log("running for", request.url);
+    // astro middleware doesn't run for static assets, even when
+    // `assets.run_worker_first` is set in wrangler.jsonc. But we can do
+    // middleware-like things here.
+    // The astro cloudflare `handler` will deal with static asset requests but
+    // bails out early without running middleware.
+    //
+    // Putting this here means it will run for *anything* where the worker runs,
+    // which includes:
+    // * server-rendered pages
+    // * live endpoints
+    // * static assets that are included in `assets.run_worker_first`
+    return addPTerryHeader(request, response);
   },
   async queue(batch, _env) {
     let messages = JSON.stringify(batch.messages);
