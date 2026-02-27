2492cfc734e15c1e4eb273c69502afa43b5dde62
Neil de Carteret <git@lumphammer.net>
2026-02-27 13:19:47 +0000

    comment

--- src/pages/roller/rooms/[roomName]/_components/types.ts
+++ src/pages/roller/rooms/[roomName]/_components/types.ts
@@ -1,3 +1,12 @@
 export type ConnectionStatus = "connected" | "disconnected" | "error";
 
+/**
+ * type for style object with added property
+ *
+ * usage:
+ *
+ * <element
+ *  style={{"--user-hue": userHue} satisfies UserHueStyle as UserHueStyle}
+ * >
+ */
 export type UserHueStyle = React.CSSProperties & { "--user-hue": number };
