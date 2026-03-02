c0ac9c4c353c6ffa83f1b1b69e074f09a9e265c4
Neil de Carteret <git@lumphammer.net>
2026-03-02 18:26:16 +0000

    comment

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,12 @@ import * as schema from "@/schemas/chatDB-schema";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
 import { betterAuth } from "better-auth";
 
+// see
+// https://github.com/better-auth/better-auth/issues/6766#issuecomment-3704724493
+// and
+// https://github.com/better-auth/better-auth/pull/6913
+// for why we have a funny-lookin' adapter
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "pg" or "mysql"
